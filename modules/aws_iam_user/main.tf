5517c9628963658ed2dbcd7971ac687b549bcbf4
Yuvraj <evalsocket@gmail.com>
2025-12-24 16:18:02 +0530

    Add MFA enforcement policy to IAM user module
    - Introduced a new IAM group policy to enforce MFA for users, denying all actions except specified ones if MFA is not present.
    - Added a corresponding IAM policy document to define the conditions under which access is denied, enhancing security for IAM users.

--- modules/aws_iam_user/main.tf
+++ modules/aws_iam_user/main.tf
@@ -32,6 +32,11 @@ resource "aws_iam_group_policy" "pass_role_to_self" {
   group  = aws_iam_group.group.name
 }
 
+resource "aws_iam_group_policy" "enforce_mfa" {
+  group  = aws_iam_group.group.name
+  policy = data.aws_iam_policy_document.enforce_mfa.json
+}
+
 data "aws_iam_policy_document" "pass_role_to_self" {
   statement {
     sid    = "AllowToPassSelf"
@@ -41,5 +46,27 @@ data "aws_iam_policy_document" "pass_role_to_self" {
       "iam:PassRole"
     ]
     resources = [aws_iam_user.user.arn]
+  }
+}
+
+data "aws_iam_policy_document" "enforce_mfa" {
+  statement {
+    sid    = "DenyAllExceptListedIfNoMFA"
+    effect = "Deny"
+    not_actions = [
+      "iam:CreateVirtualMFADevice",
+      "iam:EnableMFADevice",
+      "iam:GetUser",
+      "iam:ListMFADevices",
+      "iam:ListVirtualMFADevices",
+      "iam:ResyncMFADevice",
+      "sts:GetSessionToken"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "BoolIfExists"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["false"]
+    }
   }
 }
